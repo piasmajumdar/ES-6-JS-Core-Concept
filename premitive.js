23e8423ef65e6f81e0b4e4085e0eb6bd81dd3e37
piasmajumdar <piasmajumdar@gmail.com>
2026-02-27 21:58:15 +0530

    primitive non-primitive

---
+++ premitive.js
@@ -0,0 +1,16 @@
+let data = 4;
+data = 'tormuj';
+data = false;
+data = {price:22};
+data =[];
+// console.log(typeof data);
+
+
+// Primitive data type
+let a = 4;
+let b = 'tormuj';
+let c = false;
+
+// non-primitive data type
+let d = {price: 22}
+let e = [1, 2, 3, 4]
